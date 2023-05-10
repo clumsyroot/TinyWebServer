e52e6420bf11071af458be3c851b3e564063677b
clumsy <clumsyroot@outlook.com>
2023-05-10 17:10:22 +0800

    add comments

--- http/http_conn.h
+++ http/http_conn.h
@@ -1,37 +1,41 @@
 #ifndef HTTPCONNECTION_H
 #define HTTPCONNECTION_H
-#include <unistd.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/epoll.h>
-#include <fcntl.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <assert.h>
-#include <sys/stat.h>
-#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <map>
+#include <netinet/in.h>
 #include <pthread.h>
+#include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
 #include <sys/mman.h>
-#include <stdarg.h>
-#include <errno.h>
-#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/uio.h>
-#include <map>
+#include <sys/wait.h>
+#include <unistd.h>
 
-#include "../lock/locker.h"
 #include "../CGImysql/sql_connection_pool.h"
-#include "../timer/lst_timer.h"
+#include "../lock/locker.h"
 #include "../log/log.h"
+#include "../timer/lst_timer.h"
 
 class http_conn
 {
 public:
+    // 设置读取文件的名称 m_read_file 大小
     static const int FILENAME_LEN = 200;
+    // 设置读缓冲区 m_write_buf 的大小
     static const int READ_BUFFER_SIZE = 2048;
+    // 设置写缓冲区 m_write_buf 的大小
     static const int WRITE_BUFFER_SIZE = 1024;
+    // 报文的请求方法
     enum METHOD
     {
         GET = 0,
@@ -44,12 +48,14 @@ public:
         CONNECT,
         PATH
     };
+    // 主状态机的状态
     enum CHECK_STATE
     {
         CHECK_STATE_REQUESTLINE = 0,
         CHECK_STATE_HEADER,
         CHECK_STATE_CONTENT
     };
+    // 报文解析的结果
     enum HTTP_CODE
     {
         NO_REQUEST,
@@ -61,6 +67,7 @@ public:
         INTERNAL_ERROR,
         CLOSED_CONNECTION
     };
+    // 从状态机的状态
     enum LINE_STATUS
     {
         LINE_OK = 0,
